76c1bdf3ef652ee8ad9f36a408bc5aa69f91b749
Rosco Kalis <roscokalis@gmail.com>
2019-06-26 17:32:44 +0900

    Add reference plugin implementation

--- truffle-plugin-store/store.js
+++ truffle-plugin-store/store.js
@@ -1,3 +1,17 @@
+const axios = require('axios')
+
 module.exports = async (config) => {
-  console.log('hello')
+  const url = config.url || 'http://localhost:3000/artifacts'
+  const contractName = config._[1]
+  const contractsBuildDir = config.contracts_build_directory
+  const artifactPath = `${contractsBuildDir}/${contractName}.json`
+
+  const artifact = require(artifactPath)
+
+  const { data } = await axios.post(url, { artifact })
+
+  console.log(`Stored ${contractName} artifact with id ${data.insertedId}`)
+
+  const { data: count } = await axios.get(`${url}/${contractName}`)
+  console.log(`${count} ${contractName} artifacts stored in total`)
 }
